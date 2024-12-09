9adc67da669fc310a16a621b5b068d748f182c26
Samuel Suter <samuel.suter@nagra.com>
2024-12-09 14:51:58 +0100

    base debian-sshd on debian-dev

--- debian-sshd/conf/start_sshd.sh
+++ debian-sshd/conf/start_sshd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f .sshd/ssh_host_rsa_key ]; then
-  ssh-keygen -t rsa -f .sshd/ssh_host_rsa_key -N ''
+  ssh-keygen -t rsa -f .sshd/ssh_host_rsa_key -N '' >/dev/null
 fi
 
 /usr/sbin/sshd -f ~/.sshd/sshd_config -D
